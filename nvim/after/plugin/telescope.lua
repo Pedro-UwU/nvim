0d2ee567828c62212f3627e000da33f298064e5f
Pedro <pdihax@gmail.com>
2023-07-08 15:26:47 -0300

    Add telescope hidden files vision

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,3 +4,12 @@ vim.keymap.set('n', '<leader>fs',":lua require('telescope').extensions.live_grep
 vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require('telescope').setup{
+    defaults = { file_ignore_patterns = { "node_modules" }},
+    pickers = {
+        find_files = {
+            hidden = true
+        }
+    }
+}
